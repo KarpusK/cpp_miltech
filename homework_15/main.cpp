30e1f2da88821c2ae05addfe398fdacd7a973726
KarpusK <karpus1990@gmail.com>
2026-09-13 17:59:24 +0000

    Created http request to the server cppmiltech.com.ua. Successfully uploaded simulation.json file to application/json content type. X-api-key works.

--- homework_15/main.cpp
+++ homework_15/main.cpp
@@ -1,5 +1,14 @@
-#include <httplib.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "httplib.h"
+
+std::string readFile(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string content((std::istreambuf_iterator<char>(file)),
+                        std::istreambuf_iterator<char>());
+    return content;
+}
 
 int main() {
     httplib::Client cli("http://cppmiltech.com.ua");
@@ -10,21 +19,37 @@ int main() {
     { "X-API-Key", "dz12-vX7mK4qT9r2w" }
     };
 
+    // Треба просто передати файл як є
+    std::string json = readFile("simulation.json");
+
+    // Формуємо повний JSON
+    std::string body =
+        R"({
+            "studentId": "1072",
+            "testId": "T03",
+            "simulation": )"
+        + json +
+        R"(
+        })";
+
+    // POST
     auto res = cli.Post(
         "/api/dz12/results",
         headers,
-        R"({
-            "studentId": "6666",
-            "testId": "T03",
-            "simulation": {
-                // Вміст файлу simulation.json
-            }
-        })",
+        body,
         "application/json"
     );
 
+    if (res) {
+        std::cout << "HTTP status: " << res->status << "\n";
+        std::cout << "Server response:\n" << res->body << "\n";
+    } else {
+        std::cout << "POST failed\n";
+        std::cout << "Error: " << httplib::to_string(res.error()) << "\n";
+    }
+    
     // Передаємо заголовки другим аргументом у Get
-    if (auto res = cli.Get("/api/dz12/results/T03/1042", headers)) {
+    if (auto res = cli.Get("/api/dz12/results/T03/1072", headers)) {
         std::cout << res->body << std::endl;
     }
 
